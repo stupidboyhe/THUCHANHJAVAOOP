e7040a8724a54751116526495b225cb9ce84529c
stupidboyhe <dekun1210@gmail.com>
2024-05-22 16:36:53 +0700

    NBQA134_Cau4

---
+++ Bai Kiem Tra/src/NBQA134Main.java
@@ -0,0 +1,5 @@
+public class NBQA134Main {
+    public static void main(String[] args) {
+        
+    }
+}
--- Bai Kiem Tra/src/NBQA134Person.java
+++ Bai Kiem Tra/src/NBQA134Person.java
@@ -3,27 +3,21 @@ import java.util.Scanner;
 public class NBQA134Person {
     public String HoTen;
     public int Tuoi;
-    public String DiaChi;
+    public double Cao;
 
     public void nhapThongTin(){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         System.out.print("Nhap ho ten: ");
         HoTen = sc.nextLine();
-        System.out.print("Nhap dia chi: ");
-        DiaChi = sc.nextLine();
+        System.out.print("Nhap chieu cao: ");
+        Cao = sc.nextDouble();
         System.out.print("Nhap tuoi: ");
         Tuoi = sc.nextInt();
     }
 
     public void xuatThongTin(){
         System.out.println("Ho ten: " + HoTen);
-        System.out.println("Dia Chi: " + DiaChi);
+        System.out.println("Dia Chi: " + Cao);
         System.out.println("Tuoi: " + Tuoi);
-    }
-    
-    public NBQA134Person(){
-        HoTen = "Nguyen Van A";
-        DiaChi = "Thai Binh";
-        Tuoi = 18;
     }
 }
